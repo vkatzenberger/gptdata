16254c47b13461f6d559424253bf1eba9af784c9
vkatzenberger <v.katzenberger@proton.me>
2024-02-03 22:44:48 +0100

    Style css modified

--- adviser/views.py
+++ adviser/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import DocumentForm
 from .models import Document
 
@@ -9,8 +10,12 @@ def index(request):
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, 'File uploaded successfully!')
             return redirect('index')
     elif request.method == 'GET':
         form = DocumentForm()
     documents = Document.objects.all()
-    return render(request, 'adviser/index.html', {'form' : form, 'documents' : documents})
+    return render(request, 'adviser/index.html', {
+        'form': form,
+        'documents': documents,
+    })
